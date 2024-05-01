79041e8baddf633063f0a75caadb2c658e5426e1
anferico <f.cariaggi4@gmail.com>
2024-05-01 02:19:25 +0200

    add argument parsing to list-models

--- list_available_models.py
+++ list_available_models.py
@@ -1,5 +1,17 @@
+import argparse
+
 from clickbait_commit_messages import get_provider
 
 
-def list_available_models(provider_name: str = "huggingface") -> list[str]:
-    return get_provider(provider_name).list_available_models()
+def list_available_models() -> list[str]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--provider",
+        type=str,
+        default="huggingface",
+        help="The provider to use for generating the clickbaity message.",
+    )
+    args = parser.parse_args()
+
+    return get_provider(args.provider).list_available_models()

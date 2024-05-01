3c87d4c2e17b255b01c244c5cdd728b7656f1dee
anferico <f.cariaggi4@gmail.com>
2024-05-01 02:13:14 +0200

    add better error handling

--- clickbait_commit_messages/providers/groq.py
+++ clickbait_commit_messages/providers/groq.py
@@ -1,13 +1,37 @@
 import os
 from functools import lru_cache
+from typing import Any
 
-from groq import Groq
+import groq
 
 from clickbait_commit_messages.providers import (
     BaseProvider,
     ProviderInitializationError,
     register_provider,
 )
+
+
+def groq_api_call(default_return_value: Any):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except (
+                groq.APIConnectionError,
+                groq.RateLimitError,
+                groq.APIStatusError,
+            ) as e:
+                if isinstance(e, groq.APIConnectionError):
+                    print("Groq: server could not be reached.")
+                if isinstance(e, groq.RateLimitError):
+                    print("Groq: rate limit exceeded.")
+                if isinstance(e, groq.APIStatusError):
+                    print("Groq: non-200 status code received.")
+                return default_return_value
+
+        return wrapper
+
+    return decorator
 
 
 @register_provider("groq")
@@ -19,10 +43,11 @@ class GroqProvider(BaseProvider):
                 "Must set the GROQ_API_KEY environment variable to use the "
                 "'groq' provider."
             )
-        self.client = Groq(
+        self.client = groq.Groq(
             api_key=os.environ["GROQ_API_KEY"], timeout=3.0, max_retries=2
         )
 
+    @groq_api_call(default_return_value=[])
     @lru_cache
     def list_available_models(self) -> list[str]:
         return [
@@ -31,6 +56,7 @@ class GroqProvider(BaseProvider):
             if model.active
         ]
 
+    @groq_api_call(default_return_value="")
     @lru_cache
     def do_chat_completion(self, prompt: str, model_name: str) -> str:
         chat_completion = self.client.chat.completions.create(
--- clickbait_commit_messages/providers/huggingface.py
+++ clickbait_commit_messages/providers/huggingface.py
@@ -1,13 +1,32 @@
 import os
 from functools import lru_cache
+from typing import Any
 
-from huggingface_hub import InferenceClient
+import huggingface_hub
+from requests.exceptions import HTTPError
 
 from clickbait_commit_messages.providers import (
     BaseProvider,
     ProviderInitializationError,
     register_provider,
 )
+
+
+def hf_api_call(default_return_value: Any):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except (huggingface_hub.InferenceTimeoutError, HTTPError) as e:
+                if isinstance(e, huggingface_hub.InferenceTimeoutError):
+                    print("Hugging Face: model unavailable/request timed out.")
+                if isinstance(e, HTTPError):
+                    print("Hugging Face: non-200 status code received.")
+                return default_return_value
+
+        return wrapper
+
+    return decorator
 
 
 @register_provider("huggingface")
@@ -19,14 +38,16 @@ class HuggingFaceProvider(BaseProvider):
                 "Must set the HF_TOKEN environment variable to use the "
                 "'huggingface' provider."
             )
-        self.client = InferenceClient(
+        self.client = huggingface_hub.InferenceClient(
             token=os.environ["HF_TOKEN"], timeout=3.0
         )
 
+    @hf_api_call(default_return_value=[])
     @lru_cache
     def list_available_models(self) -> list[str]:
         return self.client.list_deployed_models()["text-generation"]
 
+    @hf_api_call(default_return_value="")
     @lru_cache
     def do_chat_completion(self, prompt: str, model_name: str) -> str:
         chat_completion = self.client.chat_completion(
